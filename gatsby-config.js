a32f17120b740b9bf703818b807b6808164612a2
Parker Eudey <parker1z@hotmail.com>
2020-03-12 18:22:08 -0700

    Basic SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,24 +1,26 @@
 const siteMetadata = {
-    title: `Parker Eudey's Blog`,
-    description: `My journey as a former mechanical designer, current programming student and prop designer`
+  title: `Parker Eudey's Blog`,
+  description: `My journey as a former mechanical designer, current programming student and prop designer`,
+  url: "https://www.parkereudey.com",
+  imrage: ""
 };
 
 module.exports = {
-    siteMetadata: siteMetadata,
-    plugins: [
-        `gatsby-plugin-styled-components`,
-        {
-            resolve: `gatsby-plugin-mdx`,
-            options: {  
-                extensions:[`.mdx`, `.md`]
-            }
+  siteMetadata: siteMetadata,
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`]
+      }
     },
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: `${__dirname}/posts`,
-                name: `posts`
-            }
-        }
-    ]
-}
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/posts`,
+        name: `posts`
+      }
+    }
+  ]
+};
